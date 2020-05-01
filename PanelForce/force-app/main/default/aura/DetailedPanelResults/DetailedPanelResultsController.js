f227f3047b6c0d91ce567d0bf2a0c70770171a28
UncleQrow <matthe.trovillo@wsu.edu>
2020-05-01 11:59:30 -0400

    Progress with Detailed View

---
+++ PanelForce/force-app/main/default/aura/DetailedPanelResults/DetailedPanelResultsController.js
@@ -0,0 +1,7 @@
+({
+	init : function(component, event, helper) {
+        console.log('here');
+		helper.getPanel(component,event,helper);
+        helper.getTopics(component,event,helper);
+	}
+})
--- PanelForce/force-app/main/default/aura/harness/harness.app
+++ PanelForce/force-app/main/default/aura/harness/harness.app
@@ -1,3 +1,3 @@
 <aura:application >
-    <c:FilterPanelResultsByName/>
+    <c:DetailedPanelResults/>
 </aura:application>
