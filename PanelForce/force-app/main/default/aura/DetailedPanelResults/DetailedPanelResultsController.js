55dd371521cc7159acf72f06620be834514703d3
UncleQrow <matthe.trovillo@wsu.edu>
2020-05-01 16:38:49 -0400

    Friday Update

--- PanelForce/force-app/main/default/aura/DetailedPanelResults/DetailedPanelResultsController.js
+++ PanelForce/force-app/main/default/aura/DetailedPanelResults/DetailedPanelResultsController.js
@@ -1,6 +1,13 @@
 ({
 	init : function(component, event, helper) {
         console.log('here');
+         component.set('v.columns', [
+            {label: 'Technology', fieldName: 'Name', type: 'text'},
+            {label: 'Max Score', fieldName: 'Max_Score__c', type: 'number'},
+            {label: 'Actual Score', fieldName: 'Actual_Score__c', type: 'number'},
+            {label: 'Repanel', fieldName: 'Repanel__c', type: 'boolean'},
+            {label: 'Comment', fieldName: 'Comments__c', type: 'text'}
+        ]);
 		helper.getPanel(component,event,helper);
         helper.getTopics(component,event,helper);
 	}
