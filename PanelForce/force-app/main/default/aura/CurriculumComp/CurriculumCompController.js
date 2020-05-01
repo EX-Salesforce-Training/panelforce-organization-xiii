927e68abaab68a8ad58b84e94b47e91e4bd0bfb4
Akeem Shane <awshane420@gmail.com>
2020-05-01 13:53:41 -0400

    Merging with master

--- PanelForce/force-app/main/default/aura/CurriculumComp/CurriculumCompController.js
+++ PanelForce/force-app/main/default/aura/CurriculumComp/CurriculumCompController.js
@@ -2,6 +2,8 @@
     
     addTopicHandler: function(component,event,helper){
 
-        let message = event.getParam("addTopics");
+        var message = event.getParam("addTopics");
         // We store the value from the event inside the attribute of Bob.
         //component.set("v.Bob", message);
+    }
+})
