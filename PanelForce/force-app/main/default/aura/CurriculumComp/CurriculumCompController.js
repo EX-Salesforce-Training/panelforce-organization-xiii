60bbcf01a89c878d50cd88a7dff9d95e2dea9511
Akeem Shane <awshane420@gmail.com>
2020-05-01 13:52:39 -0400

    Merging with integration

--- PanelForce/force-app/main/default/aura/CurriculumComp/CurriculumCompController.js
+++ PanelForce/force-app/main/default/aura/CurriculumComp/CurriculumCompController.js
@@ -2,5 +2,6 @@
     
     addTopicHandler: function(component,event,helper){
 
-    }
-})
+        let message = event.getParam("addTopics");
+        // We store the value from the event inside the attribute of Bob.
+        //component.set("v.Bob", message);
