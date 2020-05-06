16ab32bb1192285dd229d55f585858a3bee619aa
hobobumman <sergioramirez@zoho.com>
2020-05-06 14:10:52 -0700

    Co-authored-by: Hydropanda <droniton@gmail.com> Co-authored-by: dldndnjs <jenobia92@gmail.com> Co-authored-by: Vincent Espinoza <vincent.espinoza@yahoo.com>

--- PanelForce/force-app/main/default/aura/RecordPanelData/RecordPanelDataController.js
+++ PanelForce/force-app/main/default/aura/RecordPanelData/RecordPanelDataController.js
@@ -1,5 +1,20 @@
 ({
     onInit : function(component, event, helper) {
+        
+    },
 
-    }
-})
+    handleSaveRecord :function(component,event,helper){  
+        //var Duration = cmp.find("Duration__c").set("v.value", "My New Account");
+        
+        var eventfields = event.getParam('fields');
+        component.find('RecordDataID').submit(eventFields);
+        
+        var toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            "title": "Success!",
+            "message": "The record has been updated successfully."
+        });
+        toastEvent.fire();
+    }     
+});
+
