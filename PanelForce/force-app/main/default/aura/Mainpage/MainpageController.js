2765c0b7110821c963b9e45c3df9a134e88bcf6d
Hydropanda <droniton@gmail.com>
2020-05-07 17:04:25 -0400

    Nav Working Minus View Components and Record Panel Data Crash

--- PanelForce/force-app/main/default/aura/Mainpage/MainpageController.js
+++ PanelForce/force-app/main/default/aura/Mainpage/MainpageController.js
@@ -9,12 +9,14 @@
 		method.setParams({"dt" : event.getParam("datetime"), "contactId" : event.getParam("contact")});
 		method.setCallback(this, function(response) {
 			if (response.getState() === "SUCCESS") {
-				console.log("Panel retrieved: " + response.getReturnValue());
 				component.set("v.panelId", response.getReturnValue());
+				
+				try {
 				component.set("v.createPanel", false);
 				console.log(component.get("v.createPanel"));
+				} catch (e) {console.log(e)}
+
 				component.set("v.selectedTabId", "conductpanel");
-				console.log(component.get("v.selectedTabId"));
 			} else console.alert("System callout failed, state is " + response.getState() + "\nError is " + response.getError());
 		});
 		$A.enqueueAction(method);
--- PanelForce/force-app/main/default/aura/NewPanelSheet/NewPanelSheetController.js
+++ PanelForce/force-app/main/default/aura/NewPanelSheet/NewPanelSheetController.js
@@ -3,8 +3,6 @@
         event.preventDefault();
         const fields = event.getParam("fields");
 
-        component.set("v.next", event.getSource().getLocalId() === "next");
-
         component.set("v.contact", fields.Contact__c);
         component.set("v.datetime", fields.Date_Time__c);
         component.find("form").submit(fields);
@@ -19,15 +17,17 @@
         toastEvent.setParams({ "title" : "Success!", "message" : "Panel submitted successfully.", "type" : "success"});
         toastEvent.fire();
 
-        if (false && component.get("v.next")) {
-            // NOT WORKING
+        console.log(component.get("v.next"));
+        if (component.get("v.next")) {
             var forwardEvent = component.getEvent("navToRecordPanelDataDateTime");
             forwardEvent.setParams({"contact" : component.get("v.contact"), "datetime" : component.get("v.datetime")});
             forwardEvent.fire();
-            // DYING WITH WEIRD LIGHTNING ERROR XXX
         } else {
             component.set("v.refreshForm", false);
             component.set("v.refreshForm", true);
         }
+    },
+    onClick : function(component, event, helper) {
+        component.set("v.next", event.getSource().getLocalId() === "next");
     }
 })
