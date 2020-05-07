b9d21fd4d4c361e4d66d237fcc7576f0a6672051
hobobumman <sergioramirez@zoho.com>
2020-05-07 14:21:34 -0700

    Co-authored-by: Vincent Espinoza <vincent.espinoza@yahoo.com> Co-authored-by: dldndnjs <jenobia92@gmail.com>

--- PanelForce/force-app/main/default/aura/RecordPanelData/RecordPanelDataController.js
+++ PanelForce/force-app/main/default/aura/RecordPanelData/RecordPanelDataController.js
@@ -1,13 +1,31 @@
 ({
-    onInit : function(component, event, helper) {
+    recordLoaded : function(component, event, helper) {
         
+        var action = component.get("c.getTopics");
+        var id = component.get("v.id");
+ 
+        action.setParams({
+            "panelID": "a011g00000C96YuAAJ"
+        });
+
+        action.setCallback(this, function(response){
+
+            if(response.getState() === "SUCCESS"){
+
+                var topics = response.getReturnValue();
+
+                console.log(topics);
+                component.set("v.topics",topics);
+            }
+            else{
+                console.log("Error " + response.getState());
+            }
+        });
+        $A.enqueueAction(action);
     },
 
     handleSaveRecord :function(component,event,helper){  
-        //var Duration = cmp.find("Duration__c").set("v.value", "My New Account");
         
-        var eventfields = event.getParam('fields');
-        component.find('RecordDataID').submit(eventFields);
         
         var toastEvent = $A.get("e.force:showToast");
         toastEvent.setParams({
@@ -15,6 +33,74 @@
             "message": "The record has been updated successfully."
         });
         toastEvent.fire();
-    }     
+
+        /////UPDATE THE TOPICS ASSOCIATED 
+
+        var action = component.get("c.updatePanelTopics");
+        var panelID = component.get("v.id");
+        var topics = component.get("v.topics");
+        
+        action.setParams({
+            "panelId": "a011g00000C96YuAAJ",
+            "topics" : topics
+        });
+
+        action.setCallback(this, function(response){
+            if(response.getState() === "SUCCESS"){
+                
+            }
+            else{
+                console.log("Error " + response.getState());
+            }
+        });
+        $A.enqueueAction(action);
+
+    },
+    
+    updateTopic :function(component,event,helper){ 
+        
+        var topicList = component.get("v.topics");
+        var cur = component.get("v.curriculumVal");
+        var topicObjList = component.get("v.topicObjList");
+
+        component.set("v.topicObjList",[]);
+ 
+        if(cur != ""){
+            console.log("THIS IS VALUE: " + cur);
+
+            var action = component.get("c.getTopics");
+            action.setParams({
+                "curId": cur.toString()
+            });
+            action.setCallback(this, function(response){
+                if(response.getState() === "SUCCESS"){
+                    var curic = response.getReturnValue();
+                    
+                    console.log(curic);
+                    var topicNames = curic.Topics__c.split(",");
+                    var topicScores = curic.Max_Scores__c.split(",");
+
+                    //var topic = component.get("v.topic");
+                    
+                    console.log(topicNames + " " + topicScores);
+                    for(var i=0;i<topicNames.length;i++){
+
+                        var name = topicNames[i];
+                        var score = topicScores[i];
+                        topicObjList.push(name+","+score);
+
+                    }
+
+                    component.set("v.topicObjList",topicObjList);
+                }
+                else{
+                    console.log("Error " + response.getState());
+                }
+            });
+            $A.enqueueAction(action);
+        }
+
+    }
+
 });
 
--- PanelForce/force-app/main/default/aura/RecordPanelDataTopics/RecordPanelDataTopicsController.js
+++ PanelForce/force-app/main/default/aura/RecordPanelDataTopics/RecordPanelDataTopicsController.js
@@ -1,5 +1,16 @@
 ({
-    myAction : function(component, event, helper) {
+    onInit : function(component, event, helper) {
+        /*
+        var topic = component.get("v.topicObj");
+        
 
+        var tp = topic.split(",");
+
+        var topicName = tp[0];
+        var topicScore = tp[1];
+
+        component.set("v.topicName",topicName);
+        component.set("v.topicScore",topicScore);
+        */
     }
 })
